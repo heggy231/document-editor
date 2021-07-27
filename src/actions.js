afafd17736f189ea23baa9e0ef5f92e5d6d9f446
Heggy Castaneda <heggyy@gmail.com>
2021-07-27 15:08:31 -0400

    update doc in action creator fx

---
+++ src/actions.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const ADD_DOCUMENT = "ADD_DOCUMENT";
+export const DELETE_DOCUMENT = "DELETE_DOCUMENT";
+export const UPDATE_DOCUMENT = "UPDATE_DOCUMENT";
+
+export const SET_SELECTED = "SET_SELECTED";
+
+export const SET_SEARCH = "SET_SEARCH";
+
+export function addDocument(title, content) {
+  return {
+    type: ADD_DOCUMENT,
+    payload: {
+      id: uuidv4(), // Generate a unique id
+      title,
+      content,
+    },
+  };
+}
+
+export function delDocument(id) {
+  return {
+    type: DELETE_DOCUMENT,
+    payload: {
+      id,
+    },
+  };
+}
+
+export function updateDocument(id, title, content) {
+  return {
+    type: UPDATE_DOCUMENT,
+    payload: {
+      id,
+      title,
+      content,
+    }
+  }
+}
---
+++ src/store.js
@@ -0,0 +1,7 @@
+
+const defaultState = {
+  documents: [],
+  selected: '',
+  search: ''
+}
+
