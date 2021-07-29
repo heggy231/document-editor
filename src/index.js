7e2915825caced9d6cb0d94947c6aad0391ec9bd
Heggy Castaneda <heggyy@gmail.com>
2021-07-29 10:27:50 -0400

    set store actions onto window obj

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
 // import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
+import store from './store';
+// importing all action Creators functions from `actions.js`
+import {
+  addDocument,
+  updateDocument,
+  delDocument,
+  setSelected,
+  setSearch
+} from './actions';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -10,4 +19,11 @@
 //   document.getElementById('root')
 // );
 
+// attach store, actions as props to `window` obj so that I could play around in console
+window.store = store;
+window.addDocument = addDocument;
+window.updateDocument = updateDocument;
+window.delDocument = delDocument;
+window.setSelected = setSelected;
+window.setSearch = setSearch;
 
